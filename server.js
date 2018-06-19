d7861b9cf09fe20a9e00a9da8ff930904c62b034
Tom Payne <t2thep@gamil.com>
2018-06-18 19:06:54 -0500

    Server.js looking good.

--- server.js
+++ server.js
@@ -1,18 +1,61 @@
 var express = require('express');
 var app = express();
- 
- 
-app.get('/', function(req, res) {
- 
-    res.send('Welcome to Passport with Sequelize');
- 
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var env = require('dotenv').load();
+var exphbs = require('express-handlebars')
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+//For Handlebars
+app.set('views', './views')
+app.engine('hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+
+app.get('/', function (req, res) {
+    res.render();
 });
- 
- 
-app.listen(5000, function(err) {
- 
+
+var routes = require('./routes/api-routes');
+
+app.use(routes);
+
+//Models
+var models = require("./models/index.js");
+
+//Routes
+var authRoute = require('./routes/auth.js')(app, passport);
+
+
+
+//load passport strategies
+require('./config/passport/passport.js')(passport, models.User);
+
+//Sync Database
+models.sequelize.sync({}).then(function () {
+    console.log('Nice! Database looks fine')
+
+}).catch(function (err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+
+app.use(express.static('public'));
+app.use(express.static('controllers'));
+
+app.listen(5000, function (err) {
     if (!err)
-        console.log("Site is live");
+    console.log("Site is live");
     else console.log(err)
- 
+
 });
+
